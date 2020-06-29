fca4477bc6168cfd0a0925fa8152df870e72aa0d
Aidan King <aidan.king151@gmail.com>
2020-06-29 20:47:05 +0100

    integration test exercise

--- client/src/__tests__/app.login.todo.js
+++ client/src/__tests__/app.login.todo.js
@@ -1,6 +1,60 @@
-// add a beforeEach for cleaning up state and intitializing the API
+import React from 'react'
+import axiosMock from 'axios'
+import {renderWithRouter, generate, Simulate} from 'til-client-test-utils'
+import {init as initAPI} from '../utils/api'
+import App from '../app'
+
+beforeEach(() => {
+  window.localStorage.removeItem('token')
+  axiosMock.__mock.reset()
+  initAPI()
+})
 
 test('login as an existing user', async () => {
+  const {
+    container,
+    finishLoading,
+    getByText,
+    getByLabelText,
+    getByTestId,
+  } = renderWithRouter(<App />)
+
+  await finishLoading()
+
+  const leftClick = {button: 0}
+  Simulate.click(getByText('Login'), leftClick)
+  expect(window.location.href).toContain('login')
+
+  const fakeUser = generate.loginForm()
+  const usernameNode = getByLabelText('Username')
+  const passwordNode = getByLabelText('Password')
+  const formWrapper = container.querySelector('form')
+  usernameNode.value = fakeUser.username
+  passwordNode.value = fakeUser.password
+
+  const {post} = axiosMock.__mock.instance
+  const token = generate.token(fakeUser)
+  post.mockImplementationOnce(() =>
+    Promise.resolve({
+      data: {user: {...fakeUser, token}},
+    }),
+  )
+
+  Simulate.submit(formWrapper)
+
+  await finishLoading()
+
+  expect(axiosMock.__mock.instance.post).toHaveBeenCalledTimes(1)
+  expect(axiosMock.__mock.instance.post).toHaveBeenCalledWith(
+    '/auth/login',
+    fakeUser,
+  )
+
+  expect(window.localStorage.getItem('token')).toBe(token)
+  expect(window.location.href).not.toContain('login')
+  expect(getByTestId('username-display').textContent).toEqual(fakeUser.username)
+  expect(getByText('Logout')).toBeTruthy()
+
   // render the app with the router provider and custom history
   //
   // wait for the app to finish loading the mocked requests
@@ -34,8 +88,8 @@ test('login as an existing user', async () => {
 /*
 http://ws.kcd.im/?ws=Testing&e=app.login&em=
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
